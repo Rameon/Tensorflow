8cf30a0e05c529093f0c12675c9047166ebe887a
Rameon <Yirameon@gmail.com>
2020-08-26 16:24:51 +0900

    Update 3.3_linear_regression.py

--- 3.3_linear_regression.py
+++ 3.3_linear_regression.py
@@ -11,31 +11,31 @@ def linear_model(x):
 
 @tf.function
 def mse_loss(y_pred, y):
-    return tf.reduce_mean(tf.square(y_pred - y))
+    return tf.reduce_mean(tf.square(y_pred - y)) # MSE Loss Fuction
 
 
-optimizer = tf.optimizers.SGD(0.01)
+optimizer = tf.optimizers.SGD(0.01) # 러닝레이트 = 0.01 로 지정함
 
 
-# 최적화를 위한 function을 정의합니다.
 @tf.function
-def train_step(x, y):
-  with tf.GradientTape() as tape:
+def train_step(x, y): # 파라미터를 업데이트 하는 연산 : train_step
+  with tf.GradientTape() as tape: # 미분
     y_pred = linear_model(x)
     loss = mse_loss(y_pred, y)
-  gradients = tape.gradient(loss, [W, b])
+  gradients = tape.gradient(loss, [W, b]) # Loss Fuction을 각각 W와 b로 편미분 함
   optimizer.apply_gradients(zip(gradients, [W, b]))
 
-# 트레이닝을 위한 입력값과 출력값을 준비합니다.
-x_train = [1, 2, 3, 4]
-y_train = [2, 4, 6, 8]
 
-# 경사하강법을 1000번 수행합니다.
-for i in range(1000):
+# 학습을 위한 트레이닝 데이터  
+x_train = [1, 2, 3, 4] # 입력 데이터
+y_train = [2, 4, 6, 8] # 출력 데이터
+
+
+for i in range(1000): # Gradient Descent를 1000번 수행함
   train_step(x_train, y_train)
 
-# 테스트를 위한 입력값을 준비합니다.
-x_test = [3.5, 5, 5.5, 6]
-# 테스트 데이터를 이용해 학습된 선형회귀 모델이 데이터의 경향성(y=2x)을 잘 학습했는지 측정합니다.
+
+x_test = [3.5, 5, 5.5, 6] # 테스트를 위한 입력 데이터
+
 # 예상되는 참값 : [7, 10, 11, 12]
 print(linear_model(x_test).numpy())
